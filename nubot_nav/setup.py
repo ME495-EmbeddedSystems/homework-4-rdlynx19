4c0e6fcc35f40b372164812fd2839a444a496ebe
rdlynx19 <pushkardave.vnit@gmail.com>
2024-11-19 22:34:17 -0600

    set up launch files

--- nubot_nav/setup.py
+++ nubot_nav/setup.py
@@ -9,13 +9,17 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml',
+                                   'launch/manual_explore.launch.xml',
+                                   'config/nav2_params.yaml',
+                                   'config/nubot_config.rviz'
+                                   ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='redhairedlynx',
     maintainer_email='pushkardave.vnit@gmail.com',
-    description='TODO: Package description',
+    description='Mapping and navigating an environment using the slam_toolbox',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
